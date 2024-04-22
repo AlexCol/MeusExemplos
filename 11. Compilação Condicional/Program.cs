71aea14a6891698909b0514f85d3d748e9407daf
Alexandre B Coletti <alexandre.coletti@controlsoft.com.br>
2024-04-22 12:06:37 -0300

    .

--- 11. Compilação Condicional/Program.cs
+++ 11. Compilação Condicional/Program.cs
@@ -9,3 +9,5 @@ Console.WriteLine(modoTeste);
 var modoTeste = "Não é teste";
 Console.WriteLine(modoTeste);
 #endif
+
+///https://www.macoratti.net/17/10/c_compcond1.htm#:~:text=A%20compila%C3%A7%C3%A3o%20condicional%20%C3%A9%20o,outras%20partes%20a%20serem%20ignoradas.
