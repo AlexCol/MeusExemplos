2f4e8fd021443acb4abc9a446586ac3148b1f5c6
Alexandre B Coletti <alexandre.coletti@controlsoft.com.br>
2024-04-22 12:05:52 -0300

    .

---
+++ 11. Compilação Condicional/Program.cs
@@ -0,0 +1,11 @@
+﻿
+#define MODO_TESTE
+
+#if MODO_TESTE
+var modoTeste = "Modo Teste";
+Console.WriteLine(modoTeste);
+
+#else
+var modoTeste = "Não é teste";
+Console.WriteLine(modoTeste);
+#endif
