b3b8d2660ddb98dd288562bec398ed1b9d7a4393
AlexCol <eu_axil@yahoo.com.br>
2024-08-28 09:52:31 -0300

    .

--- 1. AcessoBancoDeDados/ControlSoft/CriandoScaffoldComConsole/src/DataBase/DatabaseConnection.cs
+++ 1. AcessoBancoDeDados/ControlSoft/CriandoScaffoldComConsole/src/DataBase/DatabaseConnection.cs
@@ -2,31 +2,24 @@
 using FirebirdSql.Data.FirebirdClient;
 
 namespace CriandoScaffoldComConsole.Database;
-public class DatabaseConnection
-{
+public class DatabaseConnection {
   private readonly string _connectionString;
 
-  public DatabaseConnection(string connectionString)
-  {
+  public DatabaseConnection(string connectionString) {
     _connectionString = connectionString;
   }
 
-  public FbConnection GetConnection()
-  {
+  public FbConnection GetConnection() {
     return new FbConnection(_connectionString);
   }
 
-  public bool TestConnection(out string message)
-  {
-    try
-    {
+  public bool TestConnection(out string message) {
+    try {
       using var connection = GetConnection();
       connection.Open();
       message = "Conexão estabelecida com sucesso.";
       return true;
-    }
-    catch (Exception ex)
-    {
+    } catch (Exception ex) {
       message = $"Falha ao estabelecer conexão: {ex.Message}";
       return false;
     }
--- 1. AcessoBancoDeDados/ControlSoft/CriandoScaffoldComConsole/src/DataBase/DatabaseHelper.cs
+++ 1. AcessoBancoDeDados/ControlSoft/CriandoScaffoldComConsole/src/DataBase/DatabaseHelper.cs
@@ -2,10 +2,8 @@ using System.Data;
 using FirebirdSql.Data.FirebirdClient;
 
 namespace CriandoScaffoldComConsole.Database;
-public static class DatabaseHelper
-{
-    public static DataTable ExecuteQuery(FbConnection connection, string query)
-    {
+public static class DatabaseHelper {
+    public static DataTable ExecuteQuery(FbConnection connection, string query) {
         var command = new FbCommand(query, connection);
         var reader = command.ExecuteReader();
         var table = new DataTable();
