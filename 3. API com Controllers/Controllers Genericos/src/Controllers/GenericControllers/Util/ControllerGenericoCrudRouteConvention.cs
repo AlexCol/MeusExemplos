caf9949a1eb0615063389fbd75d7f98e07c4d16b
AlexCol <eu_axil@yahoo.com.br>
2024-10-10 16:07:16 -0300

    adição de comentários para futuras referencias

--- 3. API com Controllers/Controllers Genericos/src/Controllers/GenericControllers/Util/ControllerGenericoCrudRouteConvention.cs
+++ 3. API com Controllers/Controllers Genericos/src/Controllers/GenericControllers/Util/ControllerGenericoCrudRouteConvention.cs
@@ -4,31 +4,57 @@ using Microsoft.AspNetCore.Mvc.ApplicationModels;
 
 namespace Controllers_Genericos.src.Controllers.GenericControllers.Util;
 
+/// <summary>
+/// Classe que implementa uma convenção de rota para controladores genéricos de CRUD.
+/// </summary>
 public class ControllerGenericoCrudRouteConvention : IControllerModelConvention {
+
+  /// <summary>
+  /// Aplica a convenção de roteamento ao modelo do controlador.
+  /// </summary>
+  /// <param name="controller">O modelo de controlador que receberá a convenção.</param>
   public void Apply(ControllerModel controller) {
+
+    // Verifica se o controlador é do tipo genérico.
     if (controller.ControllerType.IsGenericType) {
+
+      // Obtém o tipo genérico do controlador (por exemplo, 'Produto' em 'Controller<Produto>').
       var genericType = controller.ControllerType.GenericTypeArguments[0];
 
+      // Busca o atributo personalizado 'GenericControllerCrudAttribute' definido na classe genérica.
       var customCrudAttribute = genericType.GetCustomAttribute<GenericControllerCrudAttribute>();
+
+      // Verifica se o atributo existe e possui uma rota definida.
       if (customCrudAttribute?.Route != null) {
-        var route = customCrudAttribute.Route;
-        AddRoute(controller, route);
+        var route = customCrudAttribute.Route; // Pega a rota definida no atributo.
+        AddRoute(controller, route); // Chama o método para adicionar essa rota ao controlador.
       }
 
+      // Busca o atributo personalizado 'GenericControllerBrowseAttribute' na classe genérica.
       var customBrowseAttribute = genericType.GetCustomAttribute<GenericControllerBrowseAttribute>();
+
+      // Verifica se o atributo existe e possui uma rota definida.
       if (customBrowseAttribute?.Route != null) {
-        var route = customBrowseAttribute.Route;
-        AddRoute(controller, route);
+        var route = customBrowseAttribute.Route; // Pega a rota definida no atributo.
+        AddRoute(controller, route); // Adiciona essa rota ao controlador.
       }
     }
   }
 
+  /// <summary>
+  /// Adiciona uma nova rota ao controlador, substituindo "[controller]" pelo nome do tipo genérico.
+  /// </summary>
+  /// <param name="controller">O modelo de controlador que receberá a rota.</param>
+  /// <param name="route">A rota a ser aplicada ao controlador.</param>
   private void AddRoute(ControllerModel controller, string route) {
-    var genericType = controller.ControllerType.GenericTypeArguments[0];
+    var genericType = controller.ControllerType.GenericTypeArguments[0]; // Obtém o tipo genérico do controlador.
+
+    // Substitui o placeholder "[controller]" pelo nome do tipo genérico.
     route = route.Replace("[controller]", genericType.Name);
 
+    // Adiciona um novo seletor ao controlador, com a rota configurada.
     controller.Selectors.Add(new SelectorModel {
-      AttributeRouteModel = new AttributeRouteModel(new Microsoft.AspNetCore.Mvc.RouteAttribute(route)),
+      AttributeRouteModel = new AttributeRouteModel(new Microsoft.AspNetCore.Mvc.RouteAttribute(route)), // Define a rota como um atributo.
     });
   }
 }
--- 3. API com Controllers/Controllers Genericos/src/Controllers/GenericControllers/Util/ControllerGenericoCrudTypeFeatureProvider.cs
+++ 3. API com Controllers/Controllers Genericos/src/Controllers/GenericControllers/Util/ControllerGenericoCrudTypeFeatureProvider.cs
@@ -6,28 +6,43 @@ using Microsoft.AspNetCore.Mvc.Controllers;
 
 namespace Controllers_Genericos.src.Controllers.GenericControllers.Util;
 
+/// <summary>
+/// Classe que fornece dinamicamente tipos de controladores genéricos à aplicação.
+/// </summary>
 public class ControllerGenericoCrudTypeFeatureProvider : IApplicationFeatureProvider<ControllerFeature> {
+
+  /// <summary>
+  /// Popula o recurso de controlador com controladores genéricos de CRUD e Browse, baseando-se em atributos.
+  /// </summary>
+  /// <param name="parts">Partes da aplicação ASP.NET Core.</param>
+  /// <param name="feature">O recurso de controlador a ser populado.</param>
   public void PopulateFeature(IEnumerable<ApplicationPart> parts, ControllerFeature feature) {
+
+    // Obtém o assembly atual, onde esta classe está localizada.
     var currentAssembly = typeof(ControllerGenericoCrudTypeFeatureProvider).Assembly;
 
+    // Busca todos os tipos no assembly atual que possuem o atributo 'GenericControllerCrudAttribute'.
     var candidatesCrud = currentAssembly.GetExportedTypes()
         .Where(x => x.GetCustomAttributes<GenericControllerCrudAttribute>().Any());
 
+    // Para cada candidato encontrado com o atributo CRUD, adiciona um controlador genérico de CRUD.
     foreach (var candidate in candidatesCrud) {
-      feature.Controllers.Add(
-          typeof(ControllerGenericoCrud<>)
-          .MakeGenericType(candidate)
-          .GetTypeInfo());
+      var typeInfo = typeof(ControllerGenericoCrud<>) // Define o tipo genérico de CRUD.
+                      .MakeGenericType(candidate) // Especifica o tipo para o controlador genérico.
+                      .GetTypeInfo(); // Obtém informações de tipo para adicionar ao recurso.
+      feature.Controllers.Add(typeInfo);
     }
 
+    // Busca todos os tipos no assembly atual que possuem o atributo 'GenericControllerBrowseAttribute'.
     var candidatesBrowse = currentAssembly.GetExportedTypes()
         .Where(x => x.GetCustomAttributes<GenericControllerBrowseAttribute>().Any());
 
+    // Para cada candidato encontrado com o atributo Browse, adiciona um controlador genérico de Browse.
     foreach (var candidate in candidatesBrowse) {
-      feature.Controllers.Add(
-          typeof(ControllerGenericoBrowse<>)
-          .MakeGenericType(candidate)
-          .GetTypeInfo());
+      var typeInfo = typeof(ControllerGenericoBrowse<>) // Define o tipo genérico de Browse.
+                      .MakeGenericType(candidate) // Especifica o tipo para o controlador genérico.
+                      .GetTypeInfo(); // Obtém informações de tipo para adicionar ao recurso.
+      feature.Controllers.Add(typeInfo);
     }
   }
 }
