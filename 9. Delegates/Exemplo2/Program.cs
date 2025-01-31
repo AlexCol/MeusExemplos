a34da54ac33ec1c183fdcf00a57db27ef5c66828
AlexCol <eu_axil@yahoo.com.br>
2025-01-31 18:02:52 -0300

    adicionando exemplo com delegate anomimo junto com semaphoro

---
+++ 9. Delegates/Exemplo2/Program.cs
@@ -0,0 +1,37 @@
+﻿public class MinhaClasse {
+  private readonly SemaphoreSlim _semaphore = new(1, 1);
+
+  public async Task MetodoGenericoAsync<T>(T valor) /* where T : class */
+  {
+    await ExecutarComControleAsync(async (param) => {
+      Console.WriteLine($"Método iniciou com valor: {param}");
+      await Task.Delay(1500);
+      Console.WriteLine($"Método finalizou com valor: {param}");
+    }, valor);
+  }
+
+  private async Task ExecutarComControleAsync<T>(Func<T, Task> metodo, T parametro) {
+    await _semaphore.WaitAsync();
+    try {
+      await metodo(parametro);
+    } finally {
+      _semaphore.Release();
+    }
+  }
+}
+
+class Program {
+  static async Task Main() {
+    var obj = new MinhaClasse();
+
+    var tarefas = new[]
+    {
+            obj.MetodoGenericoAsync(42),              // int
+            obj.MetodoGenericoAsync("Hello"),         // string
+            obj.MetodoGenericoAsync(3.14),            // double
+            obj.MetodoGenericoAsync(DateTime.Now)     // DateTime
+        };
+
+    await Task.WhenAll(tarefas);
+  }
+}
